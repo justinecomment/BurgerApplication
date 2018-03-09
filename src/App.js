eb579489b79e234c065e9ed83d7385f106c43f9d
Justine <justinecomment@gmail.com>
2018-03-09 19:52:34 +0100

    Initial Commit.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+import Auxx from './hoc/Auxx/Auxx';
+
+class App extends Component {
+  render () {
+    return (
+      <Auxx>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </Auxx>
+    );
+  }
+}
+
+export default App;
---
+++ src/hoc/Auxx/Auxx.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Auxx = (props) =>{
+    
+        {props.children};
+   
+}
+
+export default Auxx;
